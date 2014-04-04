daa1edf90bcc82534b1337d6f6ad7d7d65af584d
Marcus Mascord <mmascord@hotmail.com>
2014-04-04 15:55:34 +0100

    Fragment Example

---
+++ examples/simple_fragment_example/js/views/mainview.js
@@ -0,0 +1,55 @@
+main = (function() {
+       
+      var module = {
+      
+       
+        App: (function() {
+        
+					
+             return {
+                init: function() {
+                
+                    mm.App.initMessages("config/messages/messages_en.xml", "Main", function() {
+                
+                        mm.App.initCanvasXML("canvasFlax", "config/app/common.xml", "config/app/pageflow.xml", function() {
+                
+							main.App.initPressMeButtonAction();
+				
+                            // START THE APP
+                            mm.App.start();
+                            
+                        });
+                  
+                    });
+                
+                },
+				initPressMeButtonAction: function() {
+					// ADD PRESS ME BUTTON ACTION
+					mm.App.addFunction("PRESS_ME_BUTTON_ACTION", function(buttonIn, xIn, yIn) {
+						// SIMPLE HELLO WORLD ALERT
+						alert(mm.App.getMessage("MSG_HELLO_WORLD"));
+                    });
+				}
+            };
+        })()
+        
+       
+       	};
+    
+    
+    
+	return module;
+})();
+
+$(document).ready(function() {
+
+    main.App.init();
+
+});
+
+    
+
+
+
+
+
